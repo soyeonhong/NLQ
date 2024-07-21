5932dccbcd1da0baff5ca479aaedcbb32d4afe45
soyeonhong <soyeonhong@ariel-master>
2024-07-21 16:48:41 +0900

    add infuser

--- scripts/gvqa_training.sh
+++ scripts/gvqa_training.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem-per-gpu=29G
 #SBATCH --partition batch_grad
-#SBATCH -w  ariel-v8
+#SBATCH -w  ariel-v7
 #SBATCH -t 3-0
 #SBATCH -e slurm/logs/slurm-%A_%x.err
 #SBATCH -o slurm/logs/slurm-%A_%x.out
